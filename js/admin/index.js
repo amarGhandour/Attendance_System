5709ce6147d2d64ae82a7e2da7629819cfb7ae11
amarGhandour <amarghandour89@gmail.com>
2023-01-21 04:41:14 +0200

    fix bug at daily and range admin report for computation operations like late and excuse.

--- js/admin/index.js
+++ js/admin/index.js
@@ -2,21 +2,28 @@ const domain = "http://localhost:3000";
 
 function msToTime(milli) {
     let time = new Date(milli);
-    let hours = time.getUTCHours();
-    let minutes = time.getUTCMinutes();
-    let seconds = time.getUTCSeconds();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
     return hours + ":" + minutes + ":" + seconds;
 }
 
 function getLateTime(start, end) {
+
     start = start.split(":");
     end = end.split(":");
-    var startDate = new Date(0, 0, 0, start[0], start[1], 0);
-    var endDate = new Date(0, 0, 0, end[0], end[1], 0);
-    var diff = endDate.getTime() - startDate.getTime();
-    var hours = Math.floor(diff / 1000 / 60 / 60);
+
+    if (+start[0] > +end[0])
+        return "00:00";
+    else if (+start[1] > +end[1])
+        return "00:00";
+
+    let startDate = new Date(0, 0, 0, start[0], start[1], 0);
+    let endDate = new Date(0, 0, 0, end[0], end[1], 0);
+    let diff = endDate.getTime() - startDate.getTime();
+    let hours = Math.floor(diff / 1000 / 60 / 60);
     diff -= hours * 1000 * 60 * 60;
-    var minutes = Math.floor(diff / 1000 / 60);
+    let minutes = Math.floor(diff / 1000 / 60);
 
     // If using time pickers with 24 hours format, add the below line get exact hours
     if (hours < 0)
@@ -46,22 +53,25 @@ function formatDate(date) {
 
 
 function isExcuse(time) {
-    let outTime = new Date(0, 0, 0, 15, 30, 0);
+    let excuseTime = getLateTime(msToTime(time),"15:30:00");
+    let excuseTimeArr = excuseTime.split(":");
 
-    return time < outTime.getTime();
+    return Number(excuseTimeArr[1]) > 0 ? true : Number(excuseTimeArr[0]) > 0;
 }
 
 function isLate(time) {
-    let inTime = new Date(0, 0, 0, 8, 30, 0);
+    let lateTime = getLateTime("8:30:00", msToTime(time));
+    let latTimeArr = lateTime.split(":");
 
-    return time > inTime.getTime();
+    return Number(latTimeArr[1]) > 0 ? true : Number(latTimeArr[0]) > 0;
 }
 
 function isAbsence(time) {
 
-    let acceptTime = new Date(0, 0, 0, 9, 0, 0);
+    let lateTime = getLateTime("8:30:00", msToTime(time));
+    let latTimeArr = lateTime.split(":");
 
-    return time > acceptTime.getTime();
+   return Number(latTimeArr[1]) > 30 ? true : Number(latTimeArr[0]) > 0;
 }
 
 function createRow(user) {
@@ -73,12 +83,8 @@ function createRow(user) {
                 <td>${user.type === 0 ? "Admin" : user.type === 1 ? "Security" : "Employee"}</td>
                 <td>${user.verified ? "Accepted" : "Pending"}</td>
                 <td>
-                    ${!user.verified ? '<button type="button" id="approve' + user.id + '" class="btn btn-success" data-user-id="' + user.id + '" >&check;</button>' : ""}
-                    <button type="button" id="delete` + user.id + `" class="btn btn-danger" data-user-id="` + `${user.id}` + `">X</button>
-                    <button type="button" id="view" class="btn btn-info"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
-                        <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
-                        <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/>
-                    </svg></button>
+                    <button type="button" id="approve` + user.id + `" class="btn btn-success" data-user-id="` + user.id + `" >&check;</button>
+                    <button type="button" id="delete` + user.id + `" class="btn btn-danger" data-user-id="` + user.id + `">X</button>
                 </td>
             </tr>`
 
@@ -123,7 +129,8 @@ function createDailyAdminRow(user) {
     });
     if (attendToday) {
         let trElm = document.createElement('tr');
-        let lateTime = getLateTime(msToTime(attendToday.in), msToTime(attendToday.out));
+        let lateTime = getLateTime("8:30:00",msToTime(attendToday.in));
+        let lateTimeArr = lateTime.split(":");
 
         trElm.innerHTML = `<tr>
                     <td>${user.firstName} ${user.lastName}</td>
@@ -131,7 +138,7 @@ function createDailyAdminRow(user) {
                     <td>${msToTime(attendToday.in)}</td>
                     <td>${msToTime(attendToday.out)}</td>
                     <td>${lateTime}</td>
-                    <td>${Number(lateTime.slice(lateTime.length - 2)) > 30 ? "YES" : "NO"}</td>
+                    <td>${Number(lateTimeArr[1]) > 30 ? "YES" : Number(lateTimeArr[0]) > 0? "YES" : "NO" }</td>
                 </tr>`
         document.getElementById('daily-admin-id').children[1].appendChild(trElm);
     }
@@ -141,7 +148,6 @@ async function getRangeDataTable(fromDate, toDate) {
     const usersMap = new Map();
     let usersAttendances = await fetch(`${domain}/attendances?_expand=user&date_lte=${toDate}&date_gte=${fromDate}`);
 
-    // let usersAttendances = await fetch(`${domain}/attendances?_expand=user&date_lte=2023-01-17&date_gte=2023-01-16`);
     usersAttendances = await usersAttendances.json();
 
     usersAttendances.forEach((item) => {
@@ -255,7 +261,9 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     // range admin report
-    getRangeDataTable("2023-01-15", "2023-01-21").then((data) => {
+    let month = new Date().getMonth()+1;
+    month = month <= 9? `0${month}` : `${month}`;
+    getRangeDataTable(`2023-${month}-01`, `2023-${month}-30`).then((data) => {
         data.forEach((row) => {
             createRangeAdminRow(row);
         });
